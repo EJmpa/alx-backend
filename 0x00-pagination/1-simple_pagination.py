49e898eb75c4d5f999646f1203340a7fbeacef2f
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-23 00:09:40 +0000

    pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Simple Pagination
+"""
+
+import csv
+from typing import List
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Retrieve a page of data from the dataset.
+
+        Args:
+            page (int): Page number (1-indexed).
+            page_size (int): Number of items per page.
+
+        Returns:
+            List[List]: A list of rows corresponding to the requested page.
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start, end = index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start >= len(dataset):
+            return []
+
+        return dataset[start:end]
