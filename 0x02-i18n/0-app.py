4bc27d7efbe8501e92f50fef0e012c32b3c3baf8
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-11-03 23:48:07 +0000

    i18n

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+0-app.py - Basic Flask App
+"""
+
+from flask import Flask, render_template
+from typing import Any
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> Any:
+    """
+    Render the index.html template.
+    """
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run()
